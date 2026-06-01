b77fd525f9bd4f2314940d7b878bb4843dc7f1dc
Supriya Tandukar <tandukarsupriya@gmail.com>
2026-06-01 22:48:57 +0545

    Add gitignore for config file

--- js/config.js
+++
@@ -1,6 +0,0 @@
-export const CONFIG = {
-
-    YOUTUBE_API_KEY: 'AIzaSyBJJk8_3lexjCM8W5PES71J-VjZ1JblQSc', 
-    STORAGE_KEY: 'aesthetic_writer_data',
-    FOCUS_TIMEOUT_MS: 2000 // How long to wait before UI comes back in focus mode
-};
